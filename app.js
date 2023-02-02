6511c8ebdc19d9d35149a6f20b20e7e5aa9ac6d5
mrashleyball <me@mrashleyball.com>
2023-02-03 08:59:36 +1000

    0.2

---
+++ app.js
@@ -0,0 +1,15 @@
+const calculateBtn = document.querySelector("#calculateBtn");
+calculateBtn.addEventListener("click", () => {
+    const ratio = document.querySelector("#ratio").value;
+    const ratioArr = ratio.split(":");
+    const coffeeAmount = document.querySelector("#coffeeAmount").value;
+    const coffeeRatio = parseInt(ratioArr[0]);
+    const waterRatio = parseInt(ratioArr[1]);
+    const coffeeAmountGrams = coffeeAmount * coffeeRatio;
+    const waterAmountGrams = coffeeAmount * waterRatio;
+    const milkAmountGrams = (coffeeAmount * waterRatio) / 2;
+
+    document.querySelector("#coffeeAmountResult").value = coffeeAmountGrams;
+    document.querySelector("#waterAmountResult").value = waterAmountGrams;
+    document.querySelector("#milkAmountResult").value = milkAmountGrams;
+});
